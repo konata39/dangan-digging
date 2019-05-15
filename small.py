0ec262395d0c2058c63851bdcae17d92c78c4df9
konata39 <akiyamakonata@gmail.com>
2019-05-15 19:51:01 +0800

    recursive?

--- small.py
+++ small.py
@@ -33,7 +33,7 @@ def recursive_group(map,group_map,i,j,index):
 			group_flag = True
 			group_map[i-1][j] = index
 			recursive_group(map,group_map,i-1,j,index)
-	if i != 10 and map[i+1][j] == map[i][j]:
+	if i != 4 and map[i+1][j] == map[i][j]:
 		if group_map[i+1][j] == -1:
 			group_flag = True
 			group_map[i+1][j] = index
@@ -43,7 +43,7 @@ def recursive_group(map,group_map,i,j,index):
 			group_flag = True
 			group_map[i][j-1] = index
 			recursive_group(map,group_map,i,j-1,index)
-	if j != 21 and map[i][j+1] == map[i][j]:
+	if j != 4 and map[i][j+1] == map[i][j]:
 		if group_map[i][j+1] == -1:
 			group_flag = True
 			group_map[i][j+1] = index
@@ -56,12 +56,12 @@ def recursive_group(map,group_map,i,j,index):
 def group_block(map):
 	group_map = []
 	index = 1
-	for i in range(11):
+	for i in range(5):
 		group_map.append([])
-		for j in range(22):
+		for j in range(5):
 			group_map[-1].append(-1)
-	for i in range(11):
-		for j in range(22):
+	for i in range(5):
+		for j in range(5):
 			if group_map[i][j] == -1:
 				if map[i][j] == 0:
 					group_map[i][j] = 0
@@ -95,7 +95,7 @@ def delete_block(map, locate):
 				copy_map[y-1][x] = 1
 			changed_block.append([y-1,x])
 			
-		if y < 10 and copy_map[y+1][x] != 0 and [y+1, x] not in changed_block:
+		if y < 4 and copy_map[y+1][x] != 0 and [y+1, x] not in changed_block:
 			copy_map[y+1][x] = copy_map[y+1][x] + 1
 			if copy_map[y+1][x] > 4:
 				copy_map[y+1][x] = 1
@@ -107,7 +107,7 @@ def delete_block(map, locate):
 				copy_map[y][x-1] = 1
 			changed_block.append([y,x-1])
 			
-		if x < 21 and copy_map[y][x+1] != 0 and [y, x+1] not in changed_block:
+		if x < 4 and copy_map[y][x+1] != 0 and [y, x+1] not in changed_block:
 			copy_map[y][x+1] = copy_map[y][x+1] + 1
 			if copy_map[y][x+1] > 4:
 				copy_map[y][x+1] = 1
@@ -126,31 +126,13 @@ def find_total_group(map):
 	return(max(flatten(copy_map)))
 	
 #recursive to find all result
-def dig_one_block(map, group, tap_index):
+def dig_one_block(map, group_result, tap_index):
 	min_single_block = 242
 	min_block_label = 0
-	min_tap_map = []
-	min_map = []
-	min_group = []
 	
-	total_group = find_total_group(group)
-	if total_group == 0:
-		left_block = 0
-		for j in group:
-			for k in j:
-				if k == -1:
-					left_block = left_block + 1
-		tap_map = []
-		for i in range(11):
-			tap_map.append([])
-			for j in range(22):
-				tap_map[-1].append(-1)
-				
-		return map, group, tap_map, left_block
-	print(tap_index)
-	time.sleep(0.2)
+	total_group = find_total_group(group_result)
 	for i in range(1, total_group+1):
-		matches = locate_block(group, i) 
+		matches = locate_block(group_result, i) 
 		
 		#delete block and change label near block
 		temp_map = delete_block(map, matches)
@@ -158,24 +140,44 @@ def dig_one_block(map, group, tap_index):
 		#regroup
 		temp_group_result = group_block(temp_map)
 		
-		return_map, return_group, min_tap_map, temp_left_block = dig_one_block(temp_map, temp_group_result, tap_index + 1)
+		#check left block
+		temp_left_block = 0
 		
+		for j in temp_group_result:
+			for k in j:
+				if k == -1:
+					temp_left_block = temp_left_block + 1
 		if min_single_block > temp_left_block:
 			min_single_block = temp_left_block
 			min_block_label = i
-			min_map = copy.deepcopy(return_map)
-			min_group = copy.deepcopy(return_group)
+		
+	#found least single block, update to real map and group result
+	matches = locate_block(group_result, min_block_label) 
+		
+	#delete block and change label near block
+	map = delete_block(map, matches)
+
+	#regroup
+	group_result = group_block(map)
 	
-	tap_map = copy.deepcopy(min_tap_map)
-	matches = locate_block(group, min_block_label) 
+	#update to tap map to show tap order
 	for i in matches:
-		tap_map[i[0]][i[1]] = tap_index
-	return min_map, min_group, tap_map, min_single_block
+		tap_map[i[0]][i[1]] = tap_order
+	tap_order = tap_order + 1
+	
+	group_flag = False
+	for i in group_result:
+		for j in i:
+			if j == 1:
+				group_flag = True
+				break
+	
+	return min_map, min_group, min_tap_map, min_single_block
 		
 	
 #block detection
-map_rgb = cv2.imread('7.png')
-group_rgb = cv2.imread('7.png')
+map_rgb = cv2.imread('55.png')
+group_rgb = cv2.imread('55.png')
 
 template_b1 = cv2.imread('block_1.png')
 template_b2 = cv2.imread('block_2.png')
@@ -255,28 +257,28 @@ for i in range(1, len(block_locate)):
 #draw block result
 for i in block_locate:
 	if i[1] == 1:
-		cv2.rectangle(map_rgb, i[0], (i[0][0] + w_b1, i[0][1] + h_b1), (100, 100, 100), -1)
+		cv2.rectangle(map_rgb, i[0], (i[0][0] + w_b1, i[0][1] + h_b1), (40, 40, 40), -1)
 	if i[1] == 2:
 		cv2.rectangle(map_rgb, i[0], (i[0][0] + w_b2, i[0][1] + h_b2), (208, 161, 251), -1)
 	if i[1] == 3:
 		cv2.rectangle(map_rgb, i[0], (i[0][0] + w_b3, i[0][1] + h_b3), (145, 208, 235), -1)
 	if i[1] == 4:
-		cv2.rectangle(map_rgb, i[0], (i[0][0] + w_b4, i[0][1] + h_b4), (216, 202, 123), -1)
+		cv2.rectangle(map_rgb, i[0], (i[0][0] + w_b4, i[0][1] + h_b4), (46, 202, 123), -1)
 print(len(block_locate))
 cv2.imwrite('result.png', map_rgb)
 
 #build label map
 map = []
 temp = []
-for i in range(22):
+for i in range(5):
 	temp.append([])
-	for j in range(11):
-		temp[-1].append(block_locate[i*11+j])
+	for j in range(5):
+		temp[-1].append(block_locate[i*5+j])
 	temp[-1].sort()
 
-for i in range(11):
+for i in range(5):
 	map.append([])
-	for j in range(22):
+	for j in range(5):
 		map[-1].append(temp[j][i][1])
 for i in map:
 	print(i)
@@ -287,9 +289,9 @@ group_result = group_block(map)
 #break grouped block (always break group 1 blocks)
 group_flag = True
 tap_map = []
-for i in range(11):
+for i in range(5):
 	tap_map.append([])
-	for j in range(22):
+	for j in range(5):
 		tap_map[-1].append(-1)
 
 tap_order = 1
@@ -333,7 +335,7 @@ while group_flag:
 	#update to tap map to show tap order
 	for i in matches:
 		tap_map[i[0]][i[1]] = tap_order
-	temp_tap_order = tap_order + 1
+	tap_order = tap_order + 1
 	
 	group_flag = False
 	for i in group_result:
@@ -342,7 +344,7 @@ while group_flag:
 				group_flag = True
 				break
 """
-result = dig_one_block(map, group_result, 1)
+#result_map, result_group, result_tap, result_left = dig_one_block(map, group_result, 1)
 end = time.time()
 
 print("time elapsed:",end-start)
@@ -351,8 +353,8 @@ for i in tap_map:
 
 left_block = 0
 	
-for i in range(11):
-	for j in range(22):
+for i in range(5):
+	for j in range(5):
 		cv2.rectangle(group_rgb, temp[j][i][0], (temp[j][i][0][0] + w_b1, temp[j][i][0][1] + h_b1), (0, 0, 0), -1)
 		if tap_map[i][j]>0:
 			cv2.putText(group_rgb, str(tap_map[i][j]), tuple([temp[j][i][0][0], temp[j][i][0][1]+40]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 1, cv2.LINE_AA)
